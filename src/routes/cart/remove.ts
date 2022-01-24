f521588500a4135a017f0b57c9c54db89c8272d8
Siddharth Singh <simsid66@gmail.com>
2022-01-24 13:13:03 +0530

    add /cart/remove

---
+++ src/routes/cart/remove.ts
@@ -0,0 +1,28 @@
+import { Database } from "better-sqlite3";
+import { NextFunction, Request, Response } from "express";
+import { getUserCart, setUserCart } from "../../queries";
+
+export default function removeFromCart(req: Request, res: Response, next: NextFunction) {
+    console.log('here');
+    const db: Database = req.app.get('db');
+    const cart = getUserCart(db, req.session.username);
+
+    const id = req.params.uuid;
+    if (!id) return next(400);
+
+    if (cart.includes(id)) {
+        cart.splice(cart.indexOf(id), 1);
+        setUserCart(db, req.session.username, cart);
+    }
+    else {
+        return next(400);
+    }
+
+    if (req.session.lastPage === 'cart') {
+        res.redirect('/cart');
+    }
+    else {
+        res.redirect('/');
+    }
+
+}
