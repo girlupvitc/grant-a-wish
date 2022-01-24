a40db31c8199414fae240322294e82a607ec5b62
Siddharth Singh <simsid66@gmail.com>
2022-01-24 10:22:31 +0530

    implement ensureAdmin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import { Config } from "./utils";
 
 export const ensureAdmin = (req: Request, res: Response, next: NextFunction) => {
-
+    const config: Config = req.app.get('config file');
+    if (!config.ADMINS.includes(req.session.email)) {
+        next(403);
+    }
+    else {
+        next();
+    }
 }
 
 export const ensureLoggedIn = (req: Request, res: Response, next: NextFunction) => {
