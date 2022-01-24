f820298d1a65f2abe412cb436831680b145e4db3
Siddharth Singh <simsid66@gmail.com>
2022-01-24 11:36:46 +0530

    cleanup /admin

--- src/routes/admin/root.ts
+++ src/routes/admin/root.ts
@@ -2,9 +2,6 @@ import { Database } from "better-sqlite3";
 import { NextFunction, Request, Response } from "express";
 
 export default function admin(req: Request, res: Response, next: NextFunction) {
-    console.log('here');
-    const db: Database = req.app.get('db');
-    console.log('headers sent?', res.headersSent ? 'yes' : 'no');
     res.render('admin', {
         user: {
             name: req.session.name
