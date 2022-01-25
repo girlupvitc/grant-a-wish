7e5284f512ff7030b344795b2db61161fe0b6d08
Siddharth Singh <simsid66@gmail.com>
2022-01-26 04:18:12 +0530

    add wish deletion link

--- src/routes/wishes/root.ts
+++ src/routes/wishes/root.ts
@@ -1,7 +1,7 @@
 import { Database } from "better-sqlite3";
 import { NextFunction, Request, Response } from "express";
 import { getUserInfo, isValidWish } from "../../queries";
-import { CartItem } from "../../utils";
+import { CartItem, Config } from "../../utils";
 
 const getWish = (db: Database, uuid: string): CartItem => {
     const wish = db.prepare('select title, description, price, uuid, status from wishes where uuid = ?').get(uuid);
@@ -14,13 +14,15 @@ export default function viewWish(req: Request, res: Response, next: NextFunction
     if (!req.params.uuid) {
         return next(400);
     }
+    const config: Config = req.app.get('config file');
+    const isAdmin = config.ADMINS.includes(req.session.username);
 
     req.session.lastPage = `/wishes/${req.params.uuid}`;
 
     if (isValidWish(db, req.params.uuid)) {
         const wish = getWish(db, req.params.uuid);
         const options: Record<string, any> = {
-            wish,
+            wish, isAdmin
         };
         if (req.session.username) options.user = getUserInfo(db, req.session.username);
         res.render('wish', options);
