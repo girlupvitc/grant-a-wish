22ef4b3766505496cd885429e9b9ca0ae3eabdad
Siddharth Singh <simsid66@gmail.com>
2022-01-25 08:46:49 +0530

    clean up confirm-payment.ts

--- src/routes/confirm-payment.ts
+++ src/routes/confirm-payment.ts
@@ -1,7 +1,7 @@
 import { Database } from "better-sqlite3";
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { deleteOrder, getUserCart, setCartStatus, setOrderStatus, setUserCart } from "../queries";
+import { deleteOrder, getUserCart, isPendingOrder, setCartStatus, setOrderStatus, setUserCart } from "../queries";
 import { Config, PAYMENT_STATUSES } from "../utils";
 import crypto from 'crypto';
 
@@ -17,33 +17,33 @@ const verifySignature = (razorpayId: string, paymentId: string, signature: strin
 export default async function confirmPayment(req: Request, res: Response, next: NextFunction) {
     const db: Database = req.app.get('db');
     const config: Config = req.app.get('config file');
-    console.log(req.body);
+
+    const cart = getUserCart(db, req.session.username);
+
+    const cancelOrder = () => {
+        setCartStatus(db, cart, PAYMENT_STATUSES.Available);
+        deleteOrder(db, req.body.uuid);
+        res.redirect('/cart');
+    }
+
     if (!isPendingOrder(db, req.body.orderId)) {
+        cancelOrder();
         return next(StatusCodes.BAD_REQUEST);
     }
 
     if (!req.body.ok) {
-        const cart = getUserCart(db, req.session.username);
-        setCartStatus(db, cart, PAYMENT_STATUSES.Available);
-        deleteOrder(db, req.body.uuid);
-        res.redirect('/cart');
+        cancelOrder();
         return next();
     }
     else {
-        console.log('bad request?');
-        if (!verifySignature(req.body.razorpayId, req.body.paymentId, req.body.signature, config)) return next(StatusCodes.BAD_REQUEST);
-        const cart = getUserCart(db, req.session.username);
+        if (!verifySignature(req.body.razorpayId, req.body.paymentId, req.body.signature, config)) {
+            cancelOrder();
+            return next(StatusCodes.BAD_REQUEST);
+        }
         setCartStatus(db, cart, PAYMENT_STATUSES.Successful);
         setUserCart(db, req.session.username, []);
         setOrderStatus(db, req.body.orderId, PAYMENT_STATUSES.Successful);
         res.redirect('/');
         return next();
     }
-}
-
-function isPendingOrder(db: Database, orderId: string) {
-    console.log(orderId);
-    const order = db.prepare('select uuid from orders where uuid = ? and status = ?').get(orderId, PAYMENT_STATUSES.Pending);
-    console.log('******', order);
-    return !!order;
 }
